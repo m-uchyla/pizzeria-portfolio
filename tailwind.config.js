f80b957a85e4e03d06831ee9495cb1ee48b9f951
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-04-03 15:45:47 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ed7d39",
+        orangeLight: "#f0520e",
+        orangeDark: "#fda335",
+        cream: "#f0e8e4",
+        gray: "#303030"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
