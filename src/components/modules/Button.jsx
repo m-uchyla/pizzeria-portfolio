6d4c4e6c999c7cecdba5b5b82237a917e0ef677c
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-04-05 14:13:00 +0200

    Universal buttons

--- src/components/modules/Button.jsx
+++ src/components/modules/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = ({text}) => {
+const Button = ({text, background, border, color}) => {
   return (
     <>
-        <div className='bg-orange-gradient px-6 pb-1 h-[45px] rounded-full mt-6 cursor-pointer flex-center drop-shadow-xl'>
-            <button className='text-[20px] text-white font-semibold'><p className='drop-shadow-lg'>{text}</p></button>
+        <div className={`${background} ${border} grow button px-6 pb-1 h-[45px] rounded-full mt-6 cursor-pointer flex-center drop-shadow-xl`}>
+            <button className={`text-[20px] ${color} font-semibold`}>{text}</button>
         </div>
     </>
   )
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -17,8 +17,9 @@ const Hero = () => {
       <div className='flex flex-col max-w-[60%]'>
         <h1 className='xl:text-[72px] text-[62px] text-zinc-700 font-bold'>Inne pizze są dobre, <br />
             Nasza pizza jest <span className='text-gradient'>najlepsza</span>.</h1>
-        <div className='w-[310px] mt-3'>
-          <Button text="Menu"/>
+        <div className='w-[500px] mt-3 flex gap-x-2'>
+          <Button text="Zamów online" background={"bg-orange-gradient"} border={""} color={"text-white"}/>
+          <Button text="Rezerwuj stolik" background={""} border={"border-[2px] border-primary"} color={"text-primary"}/>
         </div>
       </div>
     </div>
