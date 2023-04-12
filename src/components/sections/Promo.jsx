67468e50a3d95a7d39412f9186749dad0e41eb33
witekZWitkowic <witoldadamus94@gmail.com>
2023-04-12 11:48:44 +0200

    New chilli and paragraph fixed

--- src/components/sections/Promo.jsx
+++ src/components/sections/Promo.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { promo } from '../../constants'
 import { motion } from "framer-motion";
 
 const Promo = () => {
+  
   return (
     <section>
         <h1 className='text-[68px] font-bold text-gradient text-center'>PROMOCJE</h1>
@@ -12,9 +13,9 @@ const Promo = () => {
                 <div key={item.id} className='flex even:flex-row odd:flex-row-reverse items-center justify-around'>
                   <div className='flex flex-col items-center '>
                     <p className='text-zinc-700 text-[44px] font-semibold z-10'>{item.name}</p>
-                    <p className='text-white text-[50px] z-10 ml-[140px]'>{item.amount}</p>
+                    <p className={`text-white text-[50px] z-10 ${index % 2 ? "ml-[140px]" : "mr-[150px]"}`}>{item.amount}</p>
                     <div className='absolute promo-border w-[320px] h-[320px]'/>
-                    <img src={item.img} className='max-w-[280px] max-h-[280px] mt-[-70px] z-10'/>
+                    <img src={item.img} className='max-w-[280px] max-h-[280px] mt-[-60px] z-10'/>
                   </div>
                   <div>
                     <p className='text-zinc-700 text-[26px] font-light w-[400px] z-10'>{item.desc}</p>
